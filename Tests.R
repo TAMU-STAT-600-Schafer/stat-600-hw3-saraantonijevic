17b41d97e2ddc12a68bce928b854be628a96189a
Sara Antonijevic <saraant@tamu.edu>
2024-09-24 16:08:03 -0500

    Showing how the errors of training and test shift over time

--- Tests.R
+++ Tests.R
@@ -5,34 +5,37 @@ set.seed(123)
 
 #Creating sample data for testing
 
-n = 100 #number of samples per class
-p = 3 #number of features
+n = 100
+p = 2
 
-#Creating two normal populations
-xClass1 = matrix(rnorm(n*(p-1), mean = 0), nrow= n)
-xClass2 = matrix(rnorm(n*(p-1), mean = 2), nrow = n)
-X = rbind(cbind(1, xClass1), cbind(1, xClass2))
-y = c(rep(0, n), rep(1,n))
+#class 1: Mean (2, 2)
+X1 = matrix(rnorm(n * p, mean = 2), ncol = p)
+y0 = rep(0, n)
+
+#class 2: Mean (-2, -2)
+X2 = matrix(rnorm(n * p, mean = -2), ncol = p)
+y2 = rep(1, n)
+
+X = rbind(X1, X2)
+y = c(y0, y2)
+
+# Add intercept (column of 1's)
+X = cbind(1, X)
+
+#splitting train/test sets
+trainIdx = sample(1:(2*n), size = 0.7 * 2*n)
+XTrain = X[trainIdx, ]
+yTrain = y[trainIdx]
+
+xTest = X[-trainIdx, ]
+yTest = y[-trainIdx]
+
+#running multi-class logistic regression
+result = LRMultiClass(XTrain, yTrain, xTest, yTest, numIter = 50, eta = 0.1, lambda = 1)
+
+result$objective
+
+plot(result$objective, type = "b", col = "blue", xlab = "Iteration", ylab = "Objective Value", main = "Objective Value Over Iterations")
 
 
-# Test data with similar separation
-XtClass1 = matrix(rnorm(n * (p - 1), mean = 1), nrow = n)
-XtClass2 = matrix(rnorm(n * (p - 1), mean = 3), nrow = n)  # Increased separation for class 2
-Xt = rbind(cbind(1, XtClass1), cbind(1, XtClass2))
-yt = c(rep(0, n), rep(1, n))
-
-#random test set 
-
-numIter = 50
-eta = 0.1
-lambda = 0.1
-
-
-result = LRMultiClass(X, y, Xt, yt, numIter, eta, lambda)
-
-cat("Training Error %: ", result$error_train, "\n")
-
-cat("Testing Error %: ", result$error_test, "\n")
-
-plot(result$objective, type = "l", main = "Objective Function", ylab = "Objective Value", xlab = "Iteration")
 
