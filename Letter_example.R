24a6ce68ba57db93ea1c2342a5b57c250c201aa0
Sara Antonijevic <saraant@tamu.edu>
2024-09-24 16:20:48 -0500

    making multiple tests for out to see how it shifts with a higher lambda, and second example with a larger learning rate (eta)

--- Letter_example.R
+++ Letter_example.R
@@ -21,16 +21,28 @@ Xt = cbind(1, Xt)
 source("FunctionsLR.R")
 
 # [ToDo] Try the algorithm LRMultiClass with lambda = 1 and 50 iterations. Call the resulting object out, i.e. out = LRMultiClass(...)
-
+out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1)
 
 # The code below will draw pictures of objective function, as well as train/test error over the iterations
-plot(out$objective, type = 'o')
-plot(out$error_train, type = 'o')
-plot(out$error_test, type = 'o')
+plot(out$objective, col = "blue", type = 'o')
+plot(out$error_train,  col = "red",type = 'o')
+plot(out$error_test,  col = "green",type = 'o')
 
 # Feel free to modify the code above for different lambda/eta/numIter values to see how it affects the convergence as well as train/test errors
+out2 = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 2)
+plot(out2$objective, col = "blue", type = 'o')
+plot(out2$error_train,  col = "red",type = 'o')
+plot(out2$error_test,  col = "green",type = 'o')
+
+out3 = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.01, lambda = 2)
+plot(out3$objective, col = "blue", type = 'o')
+plot(out3$error_train,  col = "red",type = 'o')
+plot(out3$error_test,  col = "green",type = 'o')
+
+
 
 # [ToDo] Use microbenchmark to time your code with lambda=1 and 50 iterations. To save time, only apply microbenchmark 5 times.
+
 
 # [ToDo] Report the median time of your code from microbenchmark above in the comments below
 
